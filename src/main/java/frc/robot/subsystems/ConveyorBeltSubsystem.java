991b5297e4a0c0e84479b06a2c0f0c3749f001bc
Johnnychen727 <1chenjoh4@hdsb.ca>
2022-02-16 16:27:43 -0500

    minor error

--- src/main/java/frc/robot/subsystems/ConveyorBeltSubsystem.java
+++ src/main/java/frc/robot/subsystems/ConveyorBeltSubsystem.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
