36e477ee1495516d0fd878ce15cf02a33a734ff2
steamtrail241 <72204170+steamtrail241@users.noreply.github.com>
2022-02-24 16:33:32 -0500

    start on climber

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems;
 
 public class ClimberSubsystem {
+    /* we did so much work today....*/
+    // we spent most of the time trying to open up the file and refrencing the old files from last year
     
 }
