1613e4c98423892fed55e50350c8985213f99554
Lily S <59377732+lily-s-23@users.noreply.github.com>
2022-03-06 20:47:33 -0500

    Update ConveyorBeltCommandForward.java

--- src/main/java/frc/robot/commands/ConveyorBeltCommandForward.java
+++ src/main/java/frc/robot/commands/ConveyorBeltCommandForward.java
@@ -7,11 +7,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-public class ConveyorBeltCommandFoward extends CommandBase {
+public class ConveyorBeltCommandForward extends CommandBase {
 double speed;
 
   /** Creates a new ConveyorBeltCommand. */
-  public ConveyorBeltCommandFoward() {
+  public ConveyorBeltCommandForward() {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(Robot.ConveyorBeltSubsystem);
     this.speed = 1;
